2e32c2066c70aded6096631e83a5025cd91d6639
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-10-20 07:58:55 +0700

    on tap fetch data & jsx

---
+++ ReactMainConcept/my-app/src/header.jsx
@@ -0,0 +1,16 @@
+const lastName = 'Haidilao'
+const sum = (a, b) => a + b
+
+const element = (
+    <h1>
+        hello,world!. I am {lastName}. I am{sum(10, 16)}
+    </h1>
+)
+
+//ten thuoc tinh phai chuyen thanh camelCase
+
+const element2 = (
+    <a href="google.com" tabIndex={11} className="123" id="the-a">
+        Google
+    </a>
+)
